c26a8d517d3aeac9b46ad6a77bbdb84dc58aea0f
FD00131 <sakal.patil@fulcrumdigital.com>
2023-06-05 11:03:09 +0530

    intial commit part 2

---
+++ src/app/Masters/menu-category/menu-category-form/menu-category-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-category-form',
+  templateUrl: './menu-category-form.component.html',
+  styleUrls: ['./menu-category-form.component.css']
+})
+export class MenuCategoryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Masters/menu-category/menu-category.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'app-menu-category',
+  templateUrl: './menu-category.component.html',
+  styleUrls: ['./menu-category.component.css'],
+
+})
+export class MenuCategoryComponent implements OnInit {
+
+
+  constructor() { }
+  
+  ngOnInit(): void {
+   
+  }
+
+}
